ca02ec3cc808ace81d89baa4ed2eafb229e813bf
ijay2010 <40449991+ijay2010@users.noreply.github.com>
2022-06-24 12:17:15 +0100

    new

---
+++ Zuri Javascript/assignment.js
@@ -0,0 +1,24 @@
+const firstName = "Ijeoma";
+const lastName = "Umeh";
+let myHeight = 5 + 'ft';
+var country = "Nigeria"
+
+
+function getName(firstName, lastName) {
+    console.log(firstName, lastName);
+}
+getName(firstName, lastName);
+
+/*function getName(myHeight) {
+    console.log(myHeight);
+}
+getName(myHeight);*/
+function getHeight(myHeight) {
+    console.log(myHeight);
+}
+getHeight(myHeight);
+
+function getCountry(country) {
+    console.log(country);
+}
+getCountry(country);
